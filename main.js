f92147fc17d8d081934ce01e6c5cf9acfcb33168
na5r <145583660+na5r@users.noreply.github.com>
2026-06-21 14:21:06 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebas
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
-const firebaseConfig = { /* YOUR CONFIG HERE */ };
+const firebaseConfig = { apiKey: "AIzaSyAdSeTJht66G67mMQq5oRyW0YVVffoQKWM",
+  authDomain: "kaistreams.firebaseapp.com",
+  databaseURL: "https://kaistreams-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "kaistreams",
+  storageBucket: "kaistreams.firebasestorage.app",
+  messagingSenderId: "232696634492",
+  appId: "1:232696634492:web:de0122ecc33ebe71237df9",
+  measurementId: "G-FXLJM4LCXC" };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
