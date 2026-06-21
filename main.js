021036a672caf3f34a199274a2d6e4c26ddf036a
na5r <145583660+na5r@users.noreply.github.com>
2026-06-21 14:20:16 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
+
+const firebaseConfig = { /* YOUR CONFIG HERE */ };
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+// Logic to switch screens
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // User is logged in, show Profile Screen logic
+        console.log("Logged in!");
+    } else {
+        // User is logged out, show Login Screen logic
+        console.log("Logged out!");
+    }
+});
