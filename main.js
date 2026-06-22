e35be157529e16a043e9f1a71fb7dc15c7e0d409
na5r <145583660+na5r@users.noreply.github.com>
2026-06-22 13:34:42 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-import { renderHomeScreen } from "./home.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAdSeTJht66G67mMQq5oRyW0YVVffoQKWM",
+const config = { apiKey: "AIzaSyAdSeTJht66G67mMQq5oRyW0YVVffoQKWM",
   authDomain: "kaistreams.firebaseapp.com",
   databaseURL: "https://kaistreams-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "kaistreams",
   storageBucket: "kaistreams.firebasestorage.app",
   messagingSenderId: "232696634492",
   appId: "1:232696634492:web:de0122ecc33ebe71237df9",
-  measurementId: "G-FXLJM4LCXC"
-};
-
-initializeApp(firebaseConfig);
-renderHomeScreen();
+  measurementId: "G-FXLJM4LCXC" };
+initializeApp(config);
+import { initAuth } from "./auth.js";
+initAuth();
