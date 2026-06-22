baf917a253bad4d60228b21e8d759a755d873b11
na5r <145583660+na5r@users.noreply.github.com>
2026-06-22 13:20:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
-import { initLogin } from "./login.js";
-import { loadProfiles } from "./profiles.js";
+import { renderHomeScreen } from "./home.js";
 
-const firebaseConfig = { apiKey: "AIzaSyAdSeTJht66G67mMQq5oRyW0YVVffoQKWM",
+const firebaseConfig = {
+  apiKey: "AIzaSyAdSeTJht66G67mMQq5oRyW0YVVffoQKWM",
   authDomain: "kaistreams.firebaseapp.com",
   databaseURL: "https://kaistreams-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "kaistreams",
   storageBucket: "kaistreams.firebasestorage.app",
   messagingSenderId: "232696634492",
   appId: "1:232696634492:web:de0122ecc33ebe71237df9",
-  measurementId: "G-FXLJM4LCXC" };
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+  measurementId: "G-FXLJM4LCXC"
+};
 
-initLogin();
-onAuthStateChanged(auth, async (user) => {
-    if (user) {
-        document.getElementById('loginScreen').classList.add('hidden');
-        document.getElementById('profileScreen').classList.remove('hidden');
-        await loadProfiles(user.uid);
-    }
-});
+initializeApp(firebaseConfig);
+renderHomeScreen();
